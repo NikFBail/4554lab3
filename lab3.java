1d2ade721cfa4b3d5d9506eea9c0f72fb52e1a53
Nik Bailey <bail0301@morris.umn.edu>
2022-09-30 15:08:39 -0500

    Added padding
    Added padding method and added comments to methods

--- lab3.java
+++ lab3.java
@@ -4,7 +4,7 @@ import java.lang.Math;
 class lab3 {
 
     public static void main(String args[]) {
-        String block = "This is a test";
+        String block = "a5Z#\t";
 
         System.out.println(Arrays.toString(convertToASCII(block)));
         int[] ascii = convertToASCII(block);
@@ -12,8 +12,16 @@ class lab3 {
         System.out.println(Arrays.toString(convertToBinary(ascii)));
         String[] binary = convertToBinary(ascii);
 
+        System.out.println(Arrays.toString(padding(binary)));
+        String[] padded = padding(binary);
+
     }
 
+    /* Method for converting a string
+     * into an ascii value
+     * Stores the ascii values in
+     * an int array
+     */
     public static int[] convertToASCII(String input) {
         int[] result = new int[input.length()];
         int num = 0;
@@ -25,6 +33,11 @@ class lab3 {
         return result;
     }
 
+    /* Method for converting an integer
+     * array into binary values
+     * Stores the binary values in
+     * a string array
+     */
     public static String[] convertToBinary(int[] arr) {
         String[] result = new String[arr.length];
         for(int i = 0; i < arr.length; i++) {
@@ -32,6 +45,25 @@ class lab3 {
         }
 
         return result;
+    }
+
+    /* Method for padding binary values
+     * adds 0s to the right hand side
+     * of the binary if it is less than
+     * 7 digits long
+     */
+    public static String[] padding(String[] input) {
+        String curr;
+        for(int i = 0; i < input.length; i++) {
+            curr = input[i];
+            // As long as the length of curr is less than 7
+            while(curr.length() < 7) {
+                curr = 0 + curr;
+            }
+            input[i] = curr;
+        }
+
+        return input;
     }
 
 }
