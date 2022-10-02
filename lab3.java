88f9e10a48aecd3c70c9502857a4dede6b4d4687
Nik Bailey <bail0301@morris.umn.edu>
2022-10-02 16:34:47 -0500

    Implemented another method
    Successfully added the method to XOR the key and plaintext

--- lab3.java
+++ lab3.java
@@ -30,6 +30,9 @@ class lab3 {
         String[] keyPadded = padding(keyBinary);
         System.out.println(Arrays.toString(keyPadded));
 
+        String[] encrypt = encrypted(shifted, keyPadded);
+        System.out.println(Arrays.toString(encrypt));
+
     }
 
     /* Method for converting a string
@@ -116,13 +119,26 @@ class lab3 {
      * Takes two binary string arrays
      * and adds them together
      */
-    public static void encrypted(String[] plaintext, String[] key) {
+    public static String[] encrypted(String[] plaintext, String[] key) {
+        int k =0; // Holder for one digit in the binary string of key
+        int p = 0; // Holder for one digit in the binary string of plaintext
+        int sum = 0;
         String[] encrypted = new String[key.length];
+        // Filling the array with empty strings to it isn't filled with null values
+        Arrays.fill(encrypted, "");
         for(int i = 0; i < plaintext.length; i++) {
             for(int j = 0; j < plaintext[i].length(); j++) {
-                
+                // Gets the integer value of one digit in the binary of plaintext[i]
+                p = Integer.valueOf(plaintext[i].substring(j, j + 1));
+                // Gets the integer value of one digit in the binary of key[i]
+                k = Integer.valueOf(key[i].substring(j, j + 1));
+                sum = (p + k) % 2; // XOR two binary digits
+
+                // Converts sum to a string and adds it to encrypted[i]
+                encrypted[i] += Integer.toString(sum);
             }
         }
+        return encrypted;
     }
 
 }
