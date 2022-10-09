7768550b9c5fd9f366365f5e129f0b3c31c29c49
Nik Bailey <bail0301@morris.umn.edu>
2022-10-09 14:42:23 -0500

    Fixed CBC encryption
    CBC encryption should work properly now, need to implement decryption to check

--- CBC.java
+++ CBC.java
@@ -6,13 +6,20 @@ public class CBC {
     public static String[] iv = Conversions.binaryToString(cbcIV);
 
     /* Method for
-     * xor-ing the plaintext and the iv
-     * Same as the encryption method in
-     * the encryption class
+     * encrypting using cipher block chaining
+     * First xor the input with either 
+     * the iv (only for the first input)
+     * or the output of the the encryption
+     * of the previous input
+     * Then encrypt with the key
+     * The output is then xor-ed with the
+     * next input
      */
     public static String[] encryptCBC(String[] plaintext, String[] key) {
-        int k =0; // Holder for one digit in the binary string of key
+        int initVect =0; // Holder for one digit in the binary string of iv
+        int k = 0;
         int p = 0; // Holder for one digit in the binary string of plaintext
+        int encrypt = 0;
         int sum = 0;
         String[] encrypted = new String[plaintext.length];
 
@@ -24,18 +31,24 @@ public class CBC {
                 // Gets the integer value of one digit in the binary of plaintext[i]
                 p = Integer.valueOf(plaintext[i].substring(j, j + 1));
 
+                // Gets the integer value of one digit in the binary of key[i]
+                k = Integer.valueOf(key[i % key.length].substring(j, j + 1));
+
+                // If xor-ing with the iv, don't care about previous output
                 if(i < iv.length) {
-                    k = Integer.valueOf(CBC.iv[i % CBC.iv.length].substring(j, j + 1));
+                    // Gets integer value of one digit in the binary of initialVect[i]
+                    initVect = Integer.valueOf(CBC.iv[i % CBC.iv.length].substring(j, j + 1));
                 }
+                // If XOR-ing with the output of the previous encryption
                 else {
-                    // Gets the integer value of one digit in the binary of key[i]
-                    k = Integer.valueOf(key[i % key.length].substring(j, j + 1));
+                    // Gets integer value of one digit in the binary of encrypted[i - 1]
+                    initVect = Integer.valueOf(encrypted[i - 1].substring(j, j + 1));
                 }
+                
+                sum = XOR(p, initVect); // XORs two digits
 
-                sum = XOR(p, k); // XOR two binary digits
-
-                // Converts sum to a string and adds it to encrypted[i]
-                encrypted[i] += Integer.toString(sum);
+                encrypt = XOR(sum, k); // XORs two digits
+                encrypted[i] += Integer.toString(encrypt);
             }
         }
         return encrypted;
