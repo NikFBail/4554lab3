f5ada827126af04f848fe73672cd6f306cb9f1e0
Nik Bailey <bail0301@morris.umn.edu>
2022-10-18 10:51:20 -0500

    Confused
    Not sure how the counter on the IV works, looking into it

--- CTR.java
+++ CTR.java
@@ -1,6 +1,11 @@
 import java.util.Arrays;
 
 public class CTR {
-    private static int[] ctrIV = {1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1};
-    private static String[] iv = Conversions.binaryToString(ctrIV);
+    private static int[] ctrIV = {1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1};
+    private static String[] iv = new String[35];
+
+    // Return String[]
+    public static void formalCTRiv(int counter, int[] ctrIV) {
+        
+    }
 }
